b30179ba7cb13320b1936e131a6b64dfa11793ca
alef6 <alef65507@gmail.com>
2025-08-25 17:59:19 -0300

    Aula dada 25082025

---
+++ Estudos/Atividade_javascript/tipos_numeros.js
@@ -0,0 +1,21 @@
+const notaPrimeiroBi = 6;
+const notaSegundoBi = Number.parseInt("7");
+
+const total = notaPrimeiroBi + notaSegundoBi;
+
+console.log(total)
+/*
+
+Forma de converter string para número:
+Number.parseInt() -> Converte para número inteiro
+Number.parseFloat() -> Converte para número com ponto flutuante
+
+ao usar console.log(total.toFixed(2)) ele mostra o número com 2 casas decimais caso tenha um número muito grande
+
+usar Number.isNaN("nome da variavel") para verificar se o valor é um número ou não
+
+*/
+
+
+
+
