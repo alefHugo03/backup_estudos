e547256636b59b6040321eb3096366874186a8ed
alef6 <alef65507@gmail.com>
2025-11-26 16:53:23 -0300

    Algumas modificações

---
+++ Estudos/estudo_js2/for.js
@@ -0,0 +1,10 @@
+let contadorImpar = 0;
+let contadorPar = 0;
+
+for(let contador =0; contador <=100; contador++){
+    if(contador % 2 == 0){
+        contadorPar++;
+    } else {
+        contadorImpar++;
+    };
+};
---
+++ Faculdade/SQL/Function.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS teste_trigger;
+USE teste_trigger;
+
+DELIMITER //
+CREATE FUNCTION fc_calcular_média
+BEGIN
+
+
+END //
+DELIMITER ;
