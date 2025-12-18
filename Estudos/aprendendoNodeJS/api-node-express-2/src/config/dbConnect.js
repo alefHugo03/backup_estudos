4efc52e58d589f0411e02dbc60b4f6360089d465
alefHugo03 <alef65507@gmail.com>
2025-12-17 21:27:03 -0300

    subindo projeto

--- Estudos/aprendendoNodeJS/api-node-express-2/src/config/dbConnect.js
+++ Estudos/aprendendoNodeJS/api-node-express-2/src/config/dbConnect.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 mongoose.connect(process.env.DB_CONECT_MONGO);
 
-let db = mongoose.connection;
+const db = mongoose.connection;
 
 export default db;
--- Estudos/aprendendoNodeJS/api-node-express-2/src/models/Autor.js
+++ Estudos/aprendendoNodeJS/api-node-express-2/src/models/Autor.js
@@ -4,13 +4,13 @@ const autorSchema = new mongoose.Schema(
   {
     id: {type: String},
     nome: {type: String, required: true},
-    nacionalidade: {type: String}
+    nacionalidade: {type: String},
   },
   {
-    versionKey: false
-  }
-)
+    versionKey: false,
+  },
+);
 
-const autores = mongoose.model("autores", autorSchema)
+const autores = mongoose.model("autores", autorSchema);
 
 export default autores;
